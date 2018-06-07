6adf227c6d62e685bb6523e991e203495ad7b343
whosLooper <allen.marquez@aol.com>
2018-06-06 17:50:26 -0700

    Added "Keys" and Sounds
    Created a script, that linked to keys 1-7, when pressed it plays an audio sample.

---
+++ Piano Synth/Assets/Scripts/test_Sound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_Sound : MonoBehaviour {
+
+    public AudioSource audio;
+    public KeyCode userKey;
+
+	// Use this for initialization
+	void Start () {
+        // get component == new
+        audio = GetComponent<AudioSource>();
+
+    }
+    // user updates
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(userKey))
+        {
+            audio.Play();
+            //audio.Play(44100);
+        }
+    }
+    // Update is called once per frame
+    // outside of user control
+    void Update () {
+		
+	}
+}
