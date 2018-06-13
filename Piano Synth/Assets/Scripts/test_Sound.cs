b664b2215622ab0b7ddbad5fd31545360202579d
whosLooper <allen.marquez@aol.com>
2018-06-12 17:40:43 -0700

    Redid Keys with New Sounds
    Added a full set of 12 keys(7 white/5 black), added a new synthesized sound as the base layer. Properly formatted key size to be more symmetryic.

--- Piano Synth/Assets/Scripts/test_Sound.cs
+++ Piano Synth/Assets/Scripts/test_Sound.cs
@@ -2,29 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test_Sound : MonoBehaviour {
-
+public class test_Sound : MonoBehaviour
+{
+    private Renderer rendered;
     public AudioSource audio;
     public KeyCode userKey;
+    private bool audioPlays;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         // get component == new
         audio = GetComponent<AudioSource>();
+        rendered = GetComponent<Renderer>();
 
-    }
-    // user updates
-    void FixedUpdate()
-    {
-        if (Input.GetKeyDown(userKey))
-        {
-            audio.Play();
-            //audio.Play(44100);
-        }
     }
     // Update is called once per frame
     // outside of user control
-    void Update () {
-		
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(userKey))
+        {
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+                rendered.material.color = Color.red;
+            }
+        }
+        else if(Input.GetKeyUp(userKey))
+        {
+            rendered.material.color = Color.white;
+            audio.Stop();
+        }
+    }
 }
