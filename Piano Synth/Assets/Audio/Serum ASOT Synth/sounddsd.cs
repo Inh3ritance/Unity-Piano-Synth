cb0eb6664d740223f01865366d1e5fca35fbecc2
Inh3ritance <herreralaces@gmail.com>
2018-07-26 17:34:01 -0700

    Added GUI Voleme meter
    volume scroller added

---
+++ Piano Synth/Assets/Audio/Serum ASOT Synth/sounddsd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sounddsd : MonoBehaviour {
+
+
+    public Slider[] sliders;
+    public test_Sound[] stuff;
+	// Update is called once per frame
+	void Update () {
+        for (int j = 0; j < sliders.Length; j++) {
+            for (int i = 0; i < stuff.Length; i++) {
+                stuff[i].audio.volume = sliders[j].value;
+            }
+        }
+	}
+}
